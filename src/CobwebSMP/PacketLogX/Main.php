fa5a2771b236126aaf5a59f969bfb656dd92eebe
CobwebSMP <32712077+CobwebSMP@users.noreply.github.com>
2022-08-04 20:14:22 +0300

    pm4

--- src/CobwebSMP/PacketLogX/Main.php
+++ src/CobwebSMP/PacketLogX/Main.php
@@ -11,16 +11,18 @@ use pocketmine\event\server\DataPacketReceiveEvent;
 use pocketmine\utils\Terminal;
 
 class Main extends PluginBase implements Listener{
-       public function onEnable(){
+       public function onEnable() : void{
            $this->saveDefaultConfig();
            $this->getServer()->getPluginManager()->registerEvents($this, $this);
            file_put_contents($this->getDataFolder()."\log.log", "====================New session [".date('Y/m/d') ."] [".date('H:i:s') ."] UTC=====================\n",  FILE_APPEND);
        }
 
        public function onSend(DataPacketSendEvent $event){
-           $p = $event->getPacket()->getName();
-		   file_put_contents($this->getDataFolder()."\log.log", "[".date('H:i:s') ."] S server==>client " .$p. "\n", FILE_APPEND);
-		   echo Terminal::$COLOR_AQUA."[".date('H:i:s') ."] ".Terminal::$COLOR_GREEN."server==>client " .Terminal::$COLOR_WHITE. $p. "\n";
+		   foreach($event->getPackets() as $packet){
+				$p = $packet->getName();
+			}
+			file_put_contents($this->getDataFolder()."\log.log", "[".date('H:i:s') ."] S server==>client " .$p. "\n", FILE_APPEND);
+			echo Terminal::$COLOR_AQUA."[".date('H:i:s') ."] ".Terminal::$COLOR_GREEN."server==>client " .Terminal::$COLOR_WHITE. $p. "\n";
        }
 
        public function onReceive(DataPacketReceiveEvent $event){
